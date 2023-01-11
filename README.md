af7912c4978e6a4280e5b926699a5850b9afe8fe
PatriciaLengert <tizi.lengert@gmail.com>
2023-01-10 21:18:45 -0300

    conexão BD

--- README.md
+++ README.md
@@ -4,7 +4,14 @@
   - python -m venv venvbd
 ## Ativar venv
   - .\venvBD\Scripts\activate.ps1
+  - LINUX - source venvBD/bin/activate
 
 ## Instalar as LIBS
 ### Django
   - pip install django
+
+### pymysql
+  - pip install mysqlclient
+
+## Rodar o projeto
+  - python manage.py runserver
--- appTickets/views.py
+++ appTickets/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import connection
+
+def dictfetchall(cur): 
+    desc = cur.description 
+    return [
+        dict(zip([col[0] for col in desc], row)) 
+        for row in cur.fetchall() 
+    ] 
+
+def getTickets():
+    sql = f"""select * from tickets
+            """        
+    with connection.cursor() as cursor:
+        try:
+            cursor.execute(sql)
+            rs =  dictfetchall(cursor)
+            cursor.close()
+            return rs
+        except Exception as e:
+            cursor.close()
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    teste = getTickets()
+
+    print(teste)
+
+    dados = {
+        'teste': teste
+    }
+
+    return render(request, 'index.html', dados)
